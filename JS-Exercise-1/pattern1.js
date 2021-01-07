90b786366de1c3bf6603a78bf688739da4345ea3
Kishan Sheth <kishansheth21@gmail.com>
2021-01-07 17:00:58 +0530

    JS Exercise 1

--- JS-Exercise-1/pattern1.js
+++ JS-Exercise-1/pattern1.js
@@ -2,7 +2,7 @@ let num = prompt("Number of Rows");
 let pattern = "";
 for (let rows = 1; rows <= num; rows++) {
   for (let cols = 1; cols <= rows; cols++) {
-    pattern += "*";
+    pattern += "* ";
   }
   console.log(pattern);
   pattern = "";
